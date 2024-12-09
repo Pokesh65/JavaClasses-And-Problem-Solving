08d87b0e57fce1937f5a3d872687cda95e0ddb46
Pokesh Kumar <pokesh65@gmail.com>
2024-12-09 18:04:13 +0530

    Clasess Added

---
+++ JavaClassPractice/src/InstaConcepts/FactoryProduct/Os.java
@@ -0,0 +1,9 @@
+package InstaConcepts.FactoryProduct;
+
+//	every other subclasses must to be implements the main interface 
+//	and override it's methods inside the classes
+public interface Os {
+	
+	void spec();
+
+}
